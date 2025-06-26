96e912de3b88748a007d6529d884cd98260714be
LuzGarcia1620 <20223tn155@utez.edu.mx>
2025-06-26 12:50:27 -0600

    ADD: Endpoint lectura de usuarios sin terminar

--- backend/src/main/java/com/praga/backend/modules/users/controller/UserController.java
+++ backend/src/main/java/com/praga/backend/modules/users/controller/UserController.java
@@ -1,4 +1,26 @@
 package com.praga.backend.modules.users.controller;
 
+import com.praga.backend.modules.users.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = {"*"})
+@RequiredArgsConstructor
+@Tag(name = "users", description = "Endpoints para gestión de usuarios")
+
 public class UserController {
+    private final UserService userService;
+    @GetMapping("/allUsers")
+    @Operation(summary = "Obtener todos los usuarios", description = "Lista todos los usuarios del sistema")
+    public ResponseEntity<Object> getAllUsers() {
+        return userService.getAllUsers();
+    }
 }
--- backend/src/main/java/com/praga/backend/modules/users/service/UserService.java
+++ backend/src/main/java/com/praga/backend/modules/users/service/UserService.java
@@ -1,4 +1,34 @@
 package com.praga.backend.modules.users.service;
 
+
+import com.praga.backend.kernel.ApiResponse;
+import com.praga.backend.kernel.TypesResponse;
+import com.praga.backend.modules.users.controller.dto.UserDto;
+import com.praga.backend.modules.users.model.IUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class UserService {
+    private final IUserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public ResponseEntity<Object> getAllUsers() {
+        List<UserDto> users = userRepository.findAll()
+                .stream()
+                .map(user -> new UserDto(
+                        //falta aqi
+                ))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(
+                new ApiResponse<>(users, TypesResponse.SUCCESS, "Lista de usuarios obtenida correctamente")
+        );
+    }
 }
